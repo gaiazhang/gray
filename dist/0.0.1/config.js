3e3ac2c9913d11e913032e03816911dc68bb567c
zhangzhipeng <zhangzhipeng@didichuxing.com>
2020-10-06 11:00:58 +0800

    小流量demo

--- dist/0.0.1/config.js
+++ dist/0.0.1/config.js
@@ -1,8 +1,8 @@
 (
         function(){
           var mainConfig = {
-  "vendor": "https://es-static.xiaojukeji.com/static/web/esapp/js/chunk-vendors.8940e905.js",
-  "main": "https://es-static.xiaojukeji.com/static/web/esapp/js/main.80732b68.js"
+  "vendor": "https://github.com/gaiazhang/gray-version/dist/js/chunk-vendors.8940e905.js",
+  "main": "https://github.com/gaiazhang/gray-version/dist/js/main.84e25329.js"
 };
           loadScript(mainConfig.vendor, function() {
             loadScript(mainConfig.main);
